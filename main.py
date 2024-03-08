f8493638ceab4491fa2c5fb1de98e196274ea72d
Maroyan Putros <31706795+maroyanp@users.noreply.github.com>
2024-03-08 00:09:52 -0500

    Update main.py
    updated main.py to have basic message check

--- main.py
+++ main.py
@@ -8,5 +8,28 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 print(TOKEN)
 
+#bot setup
+intents = Intents.default()
+intents.message_content = True
 
+client = Client(intents = intents)
 
+# message functianality
+async def send_message(msg: Message, userMsg: str):
+    if not userMsg:
+        print('(USER MSG EMPTY AS intents were not enabled)')
+
+        return
+    
+    # is_private = userMsg[0]
+    # if is_private:  
+    # the same is done below in 1 line
+    
+    if is_private := userMsg[0] == '?': 
+        userMsg = userMsg[1:] # slice from question mark
+
+        try:
+            response = str(get_response(userMsg))
+            await msg.author.send(response) if is_private else msg.channel.send(response)
+        except Exception as e:
+            print(e)
