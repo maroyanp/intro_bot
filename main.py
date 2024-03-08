73b695293ddff4e7c21116281a8acc3104604bc7
Maroyan Putros <31706795+maroyanp@users.noreply.github.com>
2024-03-08 00:58:44 -0500

    Main.py
    checks users in vc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from discord import Intents, Client, Message
+from discord import *
 from responses import get_response
 
 #load our token from somehwre sage
@@ -15,11 +15,11 @@ intents.message_content = True
 client = Client(intents = intents)
 
 # message functianality
-async def send_message(msg: Message, userMsg: str):
+async def send_message(msg: Message, userMsg: str) -> None:
     if not userMsg:
         print('(USER MSG EMPTY AS intents were not enabled)')
 
-        return
+        return None
     
     # is_private = userMsg[0]
     # if is_private:  
@@ -28,8 +28,42 @@ async def send_message(msg: Message, userMsg: str):
     if is_private := userMsg[0] == '?': 
         userMsg = userMsg[1:] # slice from question mark
 
-        try:
-            response = str(get_response(userMsg))
-            await msg.author.send(response) if is_private else msg.channel.send(response)
-        except Exception as e:
-            print(e)
+    try:
+        response = str(get_response(userMsg))
+        await msg.author.send(response) if is_private else await msg.channel.send(response)
+    except Exception as e:
+        print(e)
+
+#handling startup for our bot
+@client.event
+async def on_read() -> None:
+    print(client.user, "is now running")
+
+#handiling incoming msgs
+@client.event
+async def on_message(message: Message) -> None:
+    if message.author == client.user:
+        return None
+    
+    username = str(message.author)
+    user_message = message.content
+    channel = str(message.channel)
+
+    print(f'[{channel}] {username} : "{user_message}"')
+    await send_message(message, user_message)
+
+# !!! CHECKING VOICE UPDATES
+@client.event
+async def on_voice_state_update(member, before, after) -> None:
+    if before.channel != after.channel:  # Check if the user changed voice channels
+        if after.channel:  # User joined a voice channel
+            print(f'{member.display_name} joined {after.channel.name}')
+        if before.channel:  # User left a voice channel
+            print(f'{member.display_name} left {before.channel.name}')
+
+#main entry point:
+def main() -> None:
+    client.run(token=TOKEN)
+
+if __name__ == '__main__':
+    main()
--- responses.py
+++ responses.py
@@ -1,2 +1,11 @@
-def get_response(str):
-    raise NotImplementedError("Code not here ")
+from random import choice, randint
+
+def get_response(user_input: str):
+    lowered = user_input.lower()
+
+    if lowered == '':
+        return 'you are not saying nothing'
+    elif 'hello' in lowered:
+        return 'hello there'
+    else:
+        return choice(['idk', 'nah id win'])
